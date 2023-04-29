487e41ab1b1c95766fbb9b78a4176bef3caf809b
FevenSeyfu <fevenseyfu00@gmail.com>
2023-04-29 20:40:43 +0300

    create component for quote

---
+++ src/components/Quote.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import '../Calculator.css';
+
+const Quotes = () => {
+  const category = 'humor';
+  const [quotes, setQuotes] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          headers: { 'X-Api-Key': 'cw1sCZ0TspAVMmL1VYR95A==qPbxZI6CH7KNYcWy' },
+          contentType: 'application/json',
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setQuotes(data);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [category]);
+
+  if (hasError || quotes.length === 0) {
+    return <div>Oops!Something went wrong!</div>;
+  }
+
+  if (isLoading) {
+    return <div>Please wait Loading quotes...</div>;
+  }
+};
+
+export default Quotes;
